5332088c10a2ae7f9fc34b143c50ff2b6629d98e
rikaz81 <rikaz81@gmail.com>
2017-06-08 06:10:07 +0530

    heruoke setup

--- server.js
+++ server.js
@@ -10,27 +10,27 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const dirDist = '';
+const dirDist = 'dist/bundle.js';
 
 // Point static path to dist
 //app.use(express.static(path.join(__dirname, 'dist')));
-app.use(express.static(path.join(dirDist, 'dist')));
+app.use(express.static('dist'));
 
 //app.use(express.static(path.join(__dirname, 'src')));
 
 //console.log(path.join(__dirname, 'dist'));
-console.log(path.join(dirDist, 'dist'));
+console.log('dist');
 
 //console.log(path.join(__dirname, 'src'));
 
 // Catch all other routes and return the index file
 app.get('/*', (req, res) => {
    //const index = path.join(__dirname, 'dist', 'index.html');   
-      const index = path.join(dirDist, 'dist', 'index.html');   
+      //const index = path.join(dirDist, 'dist', 'bundle.js');   
 
    //const index = path.join(__dirname, 'src', 'index.html');
 	
-  res.sendFile(index);
+  res.sendFile(dirDist);
 });
 
 /**
