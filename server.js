c15d33e79d1ca9e4e2526812a21e7bcf17ed75e3
rikaz81 <rikaz81@gmail.com>
2017-06-08 06:03:02 +0530

    heruoke setup

---
+++ server.js
@@ -0,0 +1,49 @@
+// Get dependencies
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const dirDist = '';
+
+// Point static path to dist
+//app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(dirDist, 'dist')));
+
+//app.use(express.static(path.join(__dirname, 'src')));
+
+//console.log(path.join(__dirname, 'dist'));
+console.log(path.join(dirDist, 'dist'));
+
+//console.log(path.join(__dirname, 'src'));
+
+// Catch all other routes and return the index file
+app.get('/*', (req, res) => {
+   //const index = path.join(__dirname, 'dist', 'index.html');   
+      const index = path.join(dirDist, 'dist', 'index.html');   
+
+   //const index = path.join(__dirname, 'src', 'index.html');
+	
+  res.sendFile(index);
+});
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '4200';
+app.set('port', port);
+
+
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
